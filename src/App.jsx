b206a63eacd52ebf2e3ea5c82a5976edfe2245e8
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-04-26 07:00:00 +0100

    Added placeholder image for user profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import userPlaceholder from "./assets/image-placeholder.jpg";
+
 
 const App = () => {
-  const [userImage, setUserImage] = useState(null);
+  const [userProfilePicture, setUserProfilePicture] = useState(null);
 
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
+  const handleProfilePictureChange = (event) => {
+    const selectedPicture = event.target.files[0];
+
+    if (selectedPicture) {
       const reader = new FileReader();
+      
       reader.onload = (e) => {
-        setUserImage(e.target.result);
+        setUserProfilePicture(e.target.result);
       };
-      reader.readAsDataURL(file);
+
+      reader.onerror = (error) => {
+        console.error('Error reading the file:', error);
+      };
+
+      reader.readAsDataURL(selectedPicture);
     }
   };
 
@@ -20,27 +29,27 @@ const App = () => {
       <div className="wrapper">
         <div className="left">
           <br/>
+
           <div className="userContainer">
-            <div 
-              className="userPicture" 
-              style={{ backgroundImage: `url(${userImage})` }}
-              id="userPicture"
-            ></div>
+            <div className="userPicture" style={{backgroundImage: `url(${userProfilePicture || userPlaceholder})`}} id="userPicture"></div>
+           
             <input 
               type="file" 
               id="imageInput" 
               accept="image/*" 
-              onChange={handleImageChange}
+              onChange={handleProfilePictureChange}
             />
             <div className="appName"><h2>Taskify</h2></div>
             <div className="userName"><h3>Tosin Faith</h3></div>
           </div>
+
           <ul>
             <li className="taskCategories">Personal</li>
             <li className="taskCategories">Freelance</li>
             <li className="taskCategories">Work</li>
           </ul>
         </div>
+
         <div className="right"></div> 
       </div>
     </div>
