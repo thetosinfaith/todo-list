821ee684f6d256a83d6650d1d8eccffc498ef00b
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-05-30 05:00:00 +0100

    click time and add task below

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 import userPlaceholder from "./assets/image-placeholder.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
-import { faTasks } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays, faCircle, faClock, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
   const [userProfilePicture, setUserProfilePicture] = useState(null);
   const [taskForTheDay, setTaskForTheDay] = useState("Read for 30 minutes..");
   const [isEditing, setIsEditing] = useState(false);
   const [tasks, setTasks] = useState([]);
-
+  const [selectedTaskTime, setSelectedTaskTime] = useState("");
 
   const handleProfilePictureChange = (event) => {
     const selectedPicture = event.target.files[0];
@@ -37,12 +34,32 @@ const App = () => {
   };
 
   const handleTitleChange = (event) => {
-   
     setTaskForTheDay(event.target.innerText);
   };
 
   const handleTitleBlur = () => {
     setIsEditing(false);
+  };
+
+  const handleTimeIconClick = () => {
+    const timeInput = document.getElementById("timeInput");
+    timeInput.click();
+  };
+
+  const handleTimeChange = (event) => {
+    setSelectedTaskTime(event.target.value);
+  };
+
+  const addTask = () => {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+
+    if (taskText) {
+      const newTask = { id: tasks.length + 1, text: taskText, time: selectedTaskTime };
+      setTasks([...tasks, newTask]);
+      taskInput.value = "";
+      setSelectedTaskTime("");
+    }
   };
 
   return (
@@ -71,15 +88,15 @@ const App = () => {
         <br />
         <ul className="taskCategoriesList">
           <li className="taskCategories">
-            <FontAwesomeIcon icon={faCircle} size="2xs" style={{ color: "#FFD43B", marginRight: "25px" }} />
+            <FontAwesomeIcon icon={faCircle} size="sm" style={{ color: "#FFD43B", marginRight: "25px" }} />
             <span>Add Task</span>
           </li>
           <li className="taskCategories">
-            <FontAwesomeIcon icon={faCircle} size="2xs" style={{ color: "#63E6BE", marginRight: "25px" }} />
+            <FontAwesomeIcon icon={faCircle} size="sm" style={{ color: "#63E6BE", marginRight: "25px" }} />
             <span>Edit Task</span>
           </li>
           <li className="taskCategories">
-            <FontAwesomeIcon icon={faCircle} size="2xs" style={{ color: "#74C0FC", marginRight: "25px" }} />
+            <FontAwesomeIcon icon={faCircle} size="sm" style={{ color: "#74C0FC", marginRight: "25px" }} />
             <span>Delete Task</span>
           </li>
         </ul>
@@ -100,45 +117,29 @@ const App = () => {
         <div className="write-tasks">
           <ul className="list">
             <li className="icons">
-              <FontAwesomeIcon icon={faCircle} size="2xs" style={{ color: "#FFD43B", marginRight: "25px" }} />
-              <FontAwesomeIcon icon={faCircle} size="2xs" style={{ color: "#63E6BE", marginRight: "25px" }} />
-              <FontAwesomeIcon icon={faCircle} size="2xs" style={{ color: "#74C0FC", marginRight: "25px" }} />
-
+              <FontAwesomeIcon icon={faCircle} size="sm" style={{ color: "#FFD43B", marginRight: "25px" }} />
+              <FontAwesomeIcon icon={faCircle} size="sm" style={{ color: "#63E6BE", marginRight: "25px" }} />
+              <FontAwesomeIcon icon={faCircle} size="sm" style={{ color: "#74C0FC", marginRight: "25px" }} />
               <input type="text" id="taskInput" placeholder=" What's your next task?" />
-
-              <FontAwesomeIcon icon={faClock} size="sm" style={{color: "#B197FC", marginLeft: "25px" }} />
-              <FontAwesomeIcon icon={faTasks} size="sm" style={{ color: "#B197FC", marginLeft: "25px" }} />   
+              <input type="time" id="timeInput" style={{ display: "none" }} onChange={handleTimeChange} />
+              <FontAwesomeIcon icon={faClock} size="sm" style={{ color: "#B197FC", marginLeft: "25px", cursor: "pointer" }} onClick={handleTimeIconClick} />
+              <FontAwesomeIcon icon={faCheck} size="sm" style={{ color: "#B197FC", marginLeft: "25px", cursor: "pointer" }} onClick={addTask} />   
             </li>
           </ul>
-
-          <br />
+ 
           <br />
           <br />
 
           <div className="other-tasks">
             <ul className="tasks-list">
-            <li className="icons">
-              <FontAwesomeIcon icon={faCircle} size="2xs" style={{ color: "#FFD43B", marginRight: "25px" }} />
-            </li>
+              {tasks.map(task => (
+                <li key={task.id} className="icons">
+                  <FontAwesomeIcon icon={faCircle} size="sm" style={{ color: "#FFD43B", marginRight: "25px" }} />
+                  {task.text} {task.time && `at ${task.time}`}
+                </li>
+              ))}
             </ul>
           </div>
-
-          <div className="other-tasks">
-            <ul className="tasks-list">
-            <li className="icons">
-              <FontAwesomeIcon icon={faCircle} size="2xs" style={{ color: "#63E6BE", marginRight: "25px" }} />
-            </li>
-            </ul>
-          </div>
-
-          <div className="other-tasks">
-            <ul className="tasks-list">
-            <li className="icons">
-              <FontAwesomeIcon icon={faCircle} size="2xs" style={{ color: "#63E6BE", marginRight: "25px" }} />
-            </li>
-            </ul>
-          </div>
-          <br />         
         </div>
       </div>
     </div>
