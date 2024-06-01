5a4eac290c034cb83d55f440ee2a085f98e2f956
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-06-01 02:13:03 +0100

    components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return(
+    <div className="parent-container">
+      <div className="wrapper">
+      <div className="left"></div>
+      <div className="right"></div> 
+      </div>
+    </div>
+  )
+}
+
+export default App
